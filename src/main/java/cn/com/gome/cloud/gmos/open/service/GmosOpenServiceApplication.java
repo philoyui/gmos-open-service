4c4c7f34568263a6908f9b181414e34a6f70facb
yang7551735 <yang7551735@126.com>
2018-09-13 23:03:13 +0800

    初始化

---
+++ src/main/java/cn/com/gome/cloud/gmos/open/service/GmosOpenServiceApplication.java
@@ -0,0 +1,12 @@
+package cn.com.gome.cloud.gmos.open.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmosOpenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmosOpenServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/com/gome/cloud/gmos/open/service/GmosOpenServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.gome.cloud.gmos.open.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmosOpenServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
